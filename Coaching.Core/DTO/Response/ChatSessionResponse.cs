cd51e75c146815d79a02f71c269d171ec36ea16b
MarcoPradoDev <antonio.123147@hotmail.com>
2022-05-20 17:36:42 -0500

    update response chat session

--- Coaching.Core/DTO/Response/ChatSessionResponse.cs
+++ Coaching.Core/DTO/Response/ChatSessionResponse.cs
@@ -15,9 +15,9 @@ namespace Coaching.Core.DTO.Response
         [JsonPropertyName("id")]
         public int Id { get; set; }
         [JsonPropertyName("transmitter")]
-        public UserResponse Transmitter { get; set; }
+        public int Transmitter { get; set; }
         [JsonPropertyName("receiver")]
-        public UserResponse Receiver { get; set; }
+        public int Receiver { get; set; }
         [JsonPropertyName("last_message")]
         public string LastMessage { get; set; }
         [JsonPropertyName("last_date")]
@@ -25,34 +25,34 @@ namespace Coaching.Core.DTO.Response
 
         public class Builder
         {
-            private ContactResponse dto;
-            private List<ContactResponse> collection;
+            private ChatSessionResponse dto;
+            private List<ChatSessionResponse> collection;
 
             public Builder()
             {
-                this.dto = new ContactResponse();
-                this.collection = new List<ContactResponse>();
+                this.dto = new ChatSessionResponse();
+                this.collection = new List<ChatSessionResponse>();
             }
-            public Builder(ContactResponse dto)
+            public Builder(ChatSessionResponse dto)
             {
                 this.dto = dto;
-                this.collection = new List<ContactResponse>();
+                this.collection = new List<ChatSessionResponse>();
             }
-            public Builder(List<ContactResponse> collection)
+            public Builder(List<ChatSessionResponse> collection)
             {
-                this.dto = new ContactResponse();
+                this.dto = new ChatSessionResponse();
                 this.collection = collection;
             }
 
-            public ContactResponse Build() => dto;
-            public List<ContactResponse> BuildAll() => collection;
+            public ChatSessionResponse Build() => dto;
+            public List<ChatSessionResponse> BuildAll() => collection;
 
             public static Builder From(Chat entity, int trasmitterId, string tipoConstructor = ConstantHelpers.CONSTRUCTOR_DTO_SINGLE)
             {
-                var dto = new ContactResponse();
+                var dto = new ChatSessionResponse();
                 dto.Id = entity.Id;
-                dto.Transmitter = entity.UserId1 == trasmitterId ? UserResponse.Builder.From(entity.UserId1Navigation).Build() : UserResponse.Builder.From(entity.UserId2Navigation).Build();
-                dto.Receiver = entity.UserId1 != trasmitterId ? UserResponse.Builder.From(entity.UserId1Navigation).Build() : UserResponse.Builder.From(entity.UserId2Navigation).Build();
+                dto.Transmitter = entity.UserId1 == trasmitterId ? entity.UserId1 : entity.UserId2;
+                dto.Receiver = entity.UserId1 != trasmitterId ? entity.UserId1 : entity.UserId2;
                 var chatsession = entity.ChatSession;
                 if (chatsession.Count > 0)
                 {
@@ -70,7 +70,7 @@ namespace Coaching.Core.DTO.Response
 
             public static Builder From(IEnumerable<Chat> entities, int transmitterId)
             {
-                var collection = new List<ContactResponse>();
+                var collection = new List<ChatSessionResponse>();
 
                 foreach (var entity in entities)
                     collection.Add(From(entity, transmitterId, ConstantHelpers.CONSTRUCTOR_DTO_LIST).Build());
